fe5111f44bec0338a32a62d8aefd1723c0c1a835
MisterSilvereagle <58659498+MisterSilvereagle@users.noreply.github.com>
2021-04-07 14:33:22 +0100

    En- and Decryption Software

---
+++ decrypt.py
@@ -0,0 +1,28 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+x = ""
+y = ""
+
+text = input('Bitte gib den Text ein, der entschlüsselt werden soll: ')
+
+schluessel = input('Bitte gib den Schlüssel ein: ')
+while not schluessel.isdigit():
+    print('Das ist keine gültige Zahl!')
+    schluessel = input('Bitte gib den Schlüssel ein: ')
+schluessel = 26-int(schluessel)
+
+text = text.lower().replace('ä','ae').replace('ö','oe').replace('ü','ue').replace('ß','sz').lower()
+
+for zeichen in text:
+    if zeichen in alphabet:
+        x = x+zeichen
+text = x
+
+for zeichen in text:
+    a = alphabet.index(zeichen)
+    a = a+schluessel
+    a = a%len(alphabet)
+    a = alphabet[a]
+    y = y+a
+text = y
+
+print(text)
