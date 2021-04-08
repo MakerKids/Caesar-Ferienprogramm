cb9df393e02754806de92ff9357c4c5900134c53
MisterSilvereagle <58659498+MisterSilvereagle@users.noreply.github.com>
2021-04-08 16:13:27 +0200

    Added program

--- crypt.py
+++ crypt.py
@@ -1,1 +1,39 @@
-#Coming Soon
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+x = ''
+y = ''
+
+method = input('Drücke [0] um zu verschlüsseln, [1] um zu entschlüsseln und [Enter], um abzubrechen: ')
+while not (method == '0' or method == '1' or method == ''):
+	print('Keine gültige Eingabe!')
+	method = input('Drücke [0] um zu verschlüsseln, [1] um zu entschlüsseln und [Enter], um abzubrechen: ')
+if method == '':
+	exit()
+else:
+	if method == '0':
+		a = 1
+		b = 0
+		text = input('Bitte gib den Text ein, der verschlüsselt werden soll: ')
+	else:
+		a = 0
+		b = 1
+		text = input('Bitte gib den Text ein, der entschlüsselt werden soll: ')
+
+
+schluessel = input('Bitte gib den Schlüssel ein: ')
+while not schluessel.isdigit():
+    print('Das ist keine gültige Zahl!')
+    schluessel = input('Bitte gib den Schlüssel ein: ')
+schluessel = int(schluessel)
+
+text = text.lower().replace('ä','ae').replace('ö','oe').replace('ü','ue').replace('ß','sz').lower()
+
+for zeichen in text:
+    if zeichen in alphabet:
+        x = x+zeichen
+text = x
+
+for zeichen in text:
+    y = y+alphabet[((alphabet.index(zeichen)+schluessel)*a+(alphabet.index(zeichen)-schluessel)*b)%len(alphabet)]
+text = y
+
+print(text)
